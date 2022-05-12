e3ba3d4b90ec13c291b2bf46ce2731a132e68275
EJamesssss <ejames.hegina@gmail.com>
2022-05-12 20:42:57 +0800

    removed comment

--- activity3.rb
+++ activity3.rb
@@ -1,24 +1,24 @@
-# class Student
-#     attr_accessor :name, :section, :color
-#     attr_reader :status
+class Student
+    attr_accessor :name, :section, :color
+    attr_reader :status
 
-#     def initialize(name,section,year)
-#         @name = name
-#         @section = section
-#         @year = year
-#         @status = "Passed"
-#     end
+    def initialize(name,section,year)
+        @name = name
+        @section = section
+        @year = year
+        @status = "Passed"
+    end
 
-#     def change_status_fail
-#         puts "The student failed"
-#         @status = "Failed"
-#     end
+    def change_status_fail
+        puts "The student failed"
+        @status = "Failed"
+    end
 
-#     def change_status_passed
-#         puts "The student passed"
-#         @status = "Passed"
-#     end
-# end
+    def change_status_passed
+        puts "The student passed"
+        @status = "Passed"
+    end
+end
 
 class Profile
     attr_accessor :full_name, :age, :address, :work
