2cdb305f9f7adc9891f872dc7ec2245768827b4d
EJamesssss <ejames.hegina@gmail.com>
2022-05-12 20:42:14 +0800

    ruby activity 3

--- activity3.rb
+++ activity3.rb
@@ -1,21 +1,45 @@
-class Student
-    attr_accessor :name, :section, :color
-    attr_reader :status
+# class Student
+#     attr_accessor :name, :section, :color
+#     attr_reader :status
 
-    def initialize(name,section,year)
-        @name = name
-        @section = section
-        @year = year
-        @status = "Passed"
-    end
+#     def initialize(name,section,year)
+#         @name = name
+#         @section = section
+#         @year = year
+#         @status = "Passed"
+#     end
 
-    def change_status_fail
-        puts "The student failed"
-        @status = "Failed"
-    end
+#     def change_status_fail
+#         puts "The student failed"
+#         @status = "Failed"
+#     end
 
-    def change_status_passed
-        puts "The student passed"
-        @status = "Passed"
+#     def change_status_passed
+#         puts "The student passed"
+#         @status = "Passed"
+#     end
+# end
+
+class Profile
+    attr_accessor :full_name, :age, :address, :work
+    def initialize(full_name, age, address, work)
+        @full_name = full_name
+        @age = age
+        @address = address
+        @work = work
     end
-end
+  end
+  
+  my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
+  
+  puts my_profile.full_name
+  my_profile.full_name = 'Juan Cruz'
+  
+  my_profile.age = 25
+  my_profile.work = 'Software Engineer'
+  
+  puts my_profile.full_name
+  puts my_profile.age
+  puts my_profile.work
+  puts my_profile.address
+  
