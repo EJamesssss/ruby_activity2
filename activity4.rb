ea16e3c462f60d996c3e2d26a58d45adf4fd4eb4
EJamesssss <ejames.hegina@gmail.com>
2022-05-14 09:28:23 +0800

    activity 4

--- activity4.rb
+++ activity4.rb
@@ -1,24 +1,15 @@
 class Confection
-
-    attr_accessor :flour, :baking_powder, :baking_soda, :eggs
-
-    def initialize(flour,baking_powder,baking_soda,eggs)
-        @flour = flour
-        @baking_powder = baking_powder
-        @baking_soda = baking_soda
-        @eggs = eggs
+    def prepare
+      puts 'Baking at 350 degrees for 25 minutes.'
     end
-
-end
-
-class Cupcake < Confection
-    def bake
-        puts "Frosting"
+  end
+  
+  class BananaCake < Confection
+  end
+  
+  class Cupcake < Confection
+    def prepare
+      super
+      puts 'Applying frosting'
     end
-end
-
-class Banana_Cake < Confection
-    def frost
-        puts "Baking at 350 degrees for 25 minutes"
-    end
-end
+  end
